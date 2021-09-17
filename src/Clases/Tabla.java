15c8b3f114357725751225cd5d24246efe5f2768
Fernando <Fernando@DESKTOP-98630AI>
2021-09-17 16:50:30 -0300

    Se agrego informacion hasta los 34 metros

--- src/Clases/Tabla.java
+++ src/Clases/Tabla.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class Tabla {
 
     Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
-    ArrayList a, b, c, d, e, f, g, h, i, j, k, l, m;
+    ArrayList a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v;
     int[] arrayMetros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48};
 
     public Tabla() {
@@ -30,6 +30,18 @@ public class Tabla {
         h = new ArrayList();
         i = new ArrayList();
         j = new ArrayList();
+        k = new ArrayList();
+        l = new ArrayList();
+        m = new ArrayList();
+        n = new ArrayList();
+        o = new ArrayList();
+        p = new ArrayList();
+        q = new ArrayList();
+        r = new ArrayList();
+        s = new ArrayList();
+        t = new ArrayList();
+        u = new ArrayList();
+        v = new ArrayList();
 
         a.add(5.594);
         a.add(12.377);
@@ -91,6 +103,78 @@ public class Tabla {
         j.add(17.351);
         j.add(33.238);
 
+        k.add(1.615);
+        k.add(3.573);
+        k.add(7.865);
+        k.add(15.839);
+        k.add(30.342);
+
+        l.add(1.495);
+        l.add(3.308);
+        l.add(7.281);
+        l.add(14.664);
+        l.add(28.091);
+
+        m.add(1.399);
+        m.add(3.094);
+        m.add(6.811);
+        m.add(13.717);
+        m.add(26.277);
+
+        n.add(1.319);
+        n.add(2.917);
+        n.add(6.421);
+        n.add(12.932);
+        n.add(24.774);
+
+        o.add(1.251);
+        o.add(2.768);
+        o.add(6.092);
+        o.add(12.269);
+        o.add(23.503);
+
+        p.add(1.193);
+        p.add(2.639);
+        p.add(5.808);
+        p.add(11.698);
+        p.add(22.409);
+
+        q.add(1.142);
+        q.add(2.526);
+        q.add(5.561);
+        q.add(11.200);
+        q.add(21.455);
+
+        r.add(1.097);
+        r.add(2.427);
+        r.add(5.343);
+        r.add(10.760);
+        r.add(20.613);
+
+        s.add(1.057);
+        s.add(2.339);
+        s.add(5.149);
+        s.add(10.369);
+        s.add(19.864);
+
+        t.add(1.021);
+        t.add(2.260);
+        t.add(4.974);
+        t.add(10.017);
+        t.add(19.190);
+
+        u.add(0.989);
+        u.add(2.188);
+        u.add(4.816);
+        u.add(9.699);
+        u.add(18.581);
+
+        v.add(0.959);
+        v.add(2.123);
+        v.add(4.672);
+        v.add(9.410);
+        v.add(18.026);
+
         map.put(1, a);
         map.put(2, b);
         map.put(3, c);
@@ -101,18 +185,30 @@ public class Tabla {
         map.put(8, h);
         map.put(9, i);
         map.put(10, j);
+        map.put(12, k);
+        map.put(14, l);
+        map.put(16, m);
+        map.put(18, n);
+        map.put(20, o);
+        map.put(22, p);
+        map.put(24, q);
+        map.put(26, r);
+        map.put(28, s);
+        map.put(30, t);
+        map.put(32, u);
+        map.put(34, v);
     }
 
     public String pedirDiametro(double metros, double consumo) {
         ArrayList fila;
         int indice = 0;
+        double metrosAdaptados = metros * 1.35;
         String res = "";
 
         //Levanto metros a los valores de la tabla
-        metros = haciaArriba(metros);
+        metrosAdaptados = haciaArriba(metrosAdaptados);
 
-        System.out.println("metros elegidos " + metros);
-        fila = map.get((int) metros);
+        fila = map.get((int) metrosAdaptados);
 
         while (consumo > (double) fila.get(indice)) {
             indice++;
